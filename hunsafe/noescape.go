bcd287f6db8313af4b8162a6618b8869bccad923
nikandfor <nikandfor@gmail.com>
2025-03-03 02:19:40 +0100

    hunsafe.NoEscape

---
+++ hunsafe/noescape.go
@@ -0,0 +1,23 @@
+package hunsafe
+
+import "unsafe"
+
+func NoEscapeBuffer(b []byte) []byte {
+	return *(*[]byte)(NoEscape(unsafe.Pointer(&b)))
+}
+
+func NoEscapeSlice[T any](b []T) []T {
+	return *(*[]T)(NoEscape(unsafe.Pointer(&b)))
+}
+
+// noescape hides a pointer from escape analysis.  noescape is
+// the identity function but escape analysis doesn't think the
+// output depends on the input.  noescape is inlined and currently
+// compiles down to zero instructions.
+// USE CAREFULLY!
+//
+//go:nosplit
+func NoEscape(p unsafe.Pointer) unsafe.Pointer {
+	x := uintptr(p)
+	return unsafe.Pointer(x ^ 0) //nolint:staticcheck
+}
